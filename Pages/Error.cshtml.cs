1705d45bb404fdfcb744f5ba1d94cc17107d81dd
Marniel Vosloo <gaminall@gmail.com>
2022-08-17 08:56:52 +0200

    refactor: change project root namespace

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace bank_file_parser.Pages;
+namespace BankFileParser.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
