e0614cb45d10bb5dd03e7cafcdc0314e34555996
marniel-v <gaminall@gmail.com>
2022-08-25 22:35:11 +0200

    fix: CultureInfo.Invariant on amount toDouble

--- Models/Deduction.cs
+++ Models/Deduction.cs
@@ -49,7 +49,7 @@ namespace BankFileParser.Models
                     case FIELD_AMOUNT:
                         try
                         {
-                            debitOrder.Amount = innerText == null ? null : Convert.ToDouble(innerText);
+                            debitOrder.Amount = innerText == null ? null : Convert.ToDouble(innerText, CultureInfo.InvariantCulture);
                         }
                         catch (System.Exception)
                         {
